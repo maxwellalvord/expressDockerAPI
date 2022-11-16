6d0b7e2139bdf94391917f54461a5093b39b158a
Maxwell Alvord <maxwellalvord@gmail.com>
2022-11-16 15:39:41 -0800

    create rest api using express and docker

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+app.get('/foo', function(req, res) {
+  res.json({"foo": "bar"})
+});
+
+
+//this is required to send data from forms 
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.post('/bar', function(req, res) {
+  var body = req.body;
+  console.log(req.body.foo);
+  res.send(req.body.foo);
+});
